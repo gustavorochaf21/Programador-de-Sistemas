514b17733b0d2ee123d449aab7af60069276eb7f
Gustavo Freitas <starzsbrsecundaria@gmail.com>
2024-11-15 10:51:06 -0800

    Exercicio 3 Completo

--- Exercicios/JavaScript/lib/Exercicio3Lista2.js
+++ Exercicios/JavaScript/lib/Exercicio3Lista2.js
@@ -4,4 +4,4 @@ function somar(numero1,numero2){
   var a = Number(prompt("Qual o primeiro numero?"))
   var b = Number(prompt("Qual o segundo numero?"))
   
-    console.log(somar(a,b))
+    document.write(`A SOMA ENTRE ${a} e ${b} é igual a ${somar(a,b)}` )
