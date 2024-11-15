162806338289c1e2ce0361bd99e733ed35974ec3
Gustavo Freitas <starzsbrsecundaria@gmail.com>
2024-11-15 11:04:47 -0800

    Exercicio 4 concluido

--- Exercicios/JavaScript/lib/Exercicio4Lista2.js
+++ Exercicios/JavaScript/lib/Exercicio4Lista2.js
@@ -4,4 +4,4 @@ function multiplicador(numero1,numero2){
   var numero1 = Number(prompt("Qual o primeiro numero?"))
   var numero2 = Number(prompt("Qual o segundo numero?"))
   
-  document.write(multiplicador(numero1,numero2))
+  document.write(`A multiplicacao entre ${numero1} e ${numero2} e igual a: ${multiplicador(numero1,numero2)}`)
