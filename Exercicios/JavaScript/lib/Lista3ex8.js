cdb8accc3a723e2a48b1afa905ce2e3e19aacacd
Gustavo Freitas <starzsbrsecundaria@gmail.com>
2024-11-21 17:03:24 -0300

    Exercicio 8 Lista 3

---
+++ Exercicios/JavaScript/lib/Lista3ex8.js
@@ -0,0 +1,7 @@
+function vogalOUconsoante(letra){
+    if (letra == "a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U"){
+        return "vogal"
+    } else {
+        return "consoante"
+    }
+}
