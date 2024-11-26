ef09efc5b96788f6fc1db0b2eda0a54e79e65998
Gustavo Freitas <starzsbrsecundaria@gmail.com>
2024-11-26 17:23:21 -0300

    Objeto.js

---
+++ Exercicios/JavaScript/lib/Objeto.js
@@ -0,0 +1,23 @@
+var carro1 = {
+    marca:"volkswagen",
+    modelo:"Jetta",
+    ano: 2021,
+    cor: "branco",
+    portas: 4,
+    carroceria: "sedan",
+    motor: "2.0 Turbo",
+    combustão: "flex",
+    cambio: "automatico",
+    potencia: "220 CV",
+    numero_do_chassi: "dad3213f",
+    direção: "eletrica",
+    calibragrem: {
+        traseiro: 32,
+        dianteiro: 32,
+    },
+    tração: "dianteira",
+    aro: 18,
+    local_de_fabricação: "Brasil"
+    
+}
+console.table(carro1)
