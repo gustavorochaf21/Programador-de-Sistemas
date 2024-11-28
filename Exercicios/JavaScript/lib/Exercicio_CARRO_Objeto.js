5bf7494c4f008821298c3e23aec10b418f900a59
Gustavo Freitas <starzsbrsecundaria@gmail.com>
2024-11-28 17:34:18 -0300

    Lista 5 de Exercicios

--- Exercicios/JavaScript/lib/Exercicio_CARRO_Objeto.js
+++ Exercicios/JavaScript/lib/Exercicio_CARRO_Objeto.js
@@ -1,48 +1,54 @@
 var carro1 = {
+    marca: "Volkswagen",
     modelo: "Fusca",
     cor: "Preto",
+    versao: "1600",
     Velocidade: 0,
+    acelerar: function () {
+        this.Velocidade = Math.min(this.Velocidade + 5, 80)
+    },
+    frear: function () {
+        this.Velocidade = Math.max(this.Velocidade - 13, 0)
+    },
+    Imprimir_ficha_tecnica: function () {
+        return console.table(this)
+    }
 }
 var carro2 = {
     modelo: "Porsche",
     cor: "Verde",
     Velocidade: 0,
+    acelerar: function () {
+        this.Velocidade = Math.min(this.Velocidade + 5, 110)
+    }, frear: function () {
+        this.Velocidade = Math.max(this.Velocidade - 13, 0)
+    },
 }
+console.log(carro1.Imprimir_ficha_tecnica())
+
+while (carro1.Velocidade < 80) carro1.acelerar()
+console.log(carro1.Velocidade)
+
+while (carro1.Velocidade > 60) {
+    carro1.frear()
+} console.log(carro1.Velocidade)
+
+while (carro1.Velocidade > 0) {
+    carro1.frear()
+} console.log(carro1.Velocidade)
 
 
-
-    function acelerar(carro){
-    carro.Velocidade += 5   
-}
-
-function frear(carro){
-    carro.Velocidade -= 13
-    if(carro.Velocidade < 0){ 
-        carro.Velocidade=0
-    }}
-
-    while(carro1.Velocidade < 80)acelerar(carro1)
-    console.log(carro1.Velocidade)
-    
-    while(carro1.Velocidade > 60){
-        frear(carro1)
-    }       console.log(carro1.Velocidade)
-
-    while (carro1.Velocidade > 0){
-        frear(carro1)
-    }       console.log(carro1.Velocidade)
 
 console.log("##TESTE CARRO 2")
 
-    while(carro2.Velocidade < 110) acelerar(carro2)
-        console.log(carro2.Velocidade)
+while (carro2.Velocidade < 110) carro2.acelerar();
+console.log(carro2.Velocidade)
 
-    while(carro2.Velocidade > 80) frear(carro2)
-        console.log(carro2.Velocidade)
+while (carro2.Velocidade > 80) carro2.frear();
+console.log(carro2.Velocidade)
 
-    while(carro2.Velocidade > 110) acelerar(carro2);
-         if (carro2.Velocidade > 110) carro2.Velocidade = 110
-         console.log(carro2.Velocidade)
+while (carro2.Velocidade < 110) carro2.acelerar();
+console.log(carro2.Velocidade)
 
 
 
