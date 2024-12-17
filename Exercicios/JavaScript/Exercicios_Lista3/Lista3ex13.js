374d13131a2c613dfbaa3c5057b61dfccf2791dd
Gustavo Rocha <starzsbrsecundaria@gmail.com>
2024-12-17 16:04:36 -0300

    Exercicio 13 Lista 3

--- Exercicios/JavaScript/Exercicios_Lista3/Lista3ex13.js
+++ Exercicios/JavaScript/Exercicios_Lista3/Lista3ex13.js
@@ -1,13 +1,2 @@
-function tabuadacompleta(){
-    for(var n=1; n <=10; n++){
-
-        for (var k = 0; k <= 10; k++) {
-            console.log(`${n} x ${k} = ${n * k}`)
-        console.log("================")
-            }        
-        console.log()
-        
-    }
-    return
-}
-tabuadacompleta()
+import { tabuadaCompleta } from "../lib/biblioteca.js";
+console.log(tabuadaCompleta())
