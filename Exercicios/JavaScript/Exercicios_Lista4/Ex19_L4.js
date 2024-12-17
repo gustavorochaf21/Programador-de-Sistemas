e7c1db90de25efcb1b4c6fdcbe28d93f32710a86
Gustavo Rocha <starzsbrsecundaria@gmail.com>
2024-12-17 17:06:04 -0300

    Exercicio 19 Lista 4

--- Exercicios/JavaScript/Exercicios_Lista4/Ex19_L4.js
+++ Exercicios/JavaScript/Exercicios_Lista4/Ex19_L4.js
@@ -1,9 +1,5 @@
 var palavra = "listen"
 var palavra2 = "silent"
 
-function Anagrama(texto1,texto2){
-    caabe = texto1.split('').sort().join('')
-    maca = texto2.split('').sort().join('')
-    return caabe === maca
-}
+import { Anagrama } from "../lib/biblioteca.js"
 console.log(Anagrama(palavra,palavra2))
