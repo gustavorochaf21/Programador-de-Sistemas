206243655a11eba0b5ab0a512ecfebb756428009
Gustavo Rocha <starzsbrsecundaria@gmail.com>
2024-12-17 16:15:10 -0300

    Exercicio 10 Lista 4

--- Exercicios/JavaScript/Exercicios_Lista4/Ex10_L4.js
+++ Exercicios/JavaScript/Exercicios_Lista4/Ex10_L4.js
@@ -1,8 +1,8 @@
 var cenario1 = [1, 2, 3, 4, 5], cenario2 = [2, 4, 6, 8], cenario3 = [1, 3, 5, 7]
 
- var filtraPar = (num) => num % 2 === 0;
- var soma = (A,B) => A + B ;
+import { somar } from "../lib/biblioteca.js"
+import { verificaPar } from "../lib/biblioteca.js"
 
-console.log(cenario1.filter(filtraPar).reduce(soma, 0))
-console.log(cenario2.filter(filtraPar).reduce(soma, 0))
-console.log(cenario3.filter(filtraPar).reduce(soma, 0))
+console.log(cenario1.filter(verificaPar).reduce(somar, 0))
+console.log(cenario2.filter(verificaPar).reduce(somar, 0))
+console.log(cenario3.filter(verificaPar).reduce(somar, 0))
