edd5e4b14e57097eb76adc21e112adc5c675017c
Gustavo Rocha <starzsbrsecundaria@gmail.com>
2024-12-17 16:12:04 -0300

    Exercicio 9 Lisat 4

--- Exercicios/JavaScript/Exercicios_Lista4/Ex9_L4.js
+++ Exercicios/JavaScript/Exercicios_Lista4/Ex9_L4.js
@@ -1,9 +1,9 @@
 var cenario1 = [1, 2, 3, 4, 5], cenario2 = [2, 4, 6, 8], cenario3 = [1, 3, 5, 7], cenario4 = [0], cenario5 = []
 
-var somarArray = (A, B) => A + B;
+import { somar } from "../lib/biblioteca.js"
 
-console.log(cenario1.reduce(somarArray, 0))
-console.log(cenario2.reduce(somarArray, 0))
-console.log(cenario3.reduce(somarArray, 0))
-console.log(cenario4.reduce(somarArray, 0))
-console.log(cenario5.reduce(somarArray, 0))
+console.log(cenario1.reduce(somar, 0))
+console.log(cenario2.reduce(somar, 0))
+console.log(cenario3.reduce(somar, 0))
+console.log(cenario4.reduce(somar, 0))
+console.log(cenario5.reduce(somar, 0))
