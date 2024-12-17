adc3573b86805fd11a081abaafb2da45e400ad66
Gustavo Rocha <starzsbrsecundaria@gmail.com>
2024-12-17 15:18:57 -0300

    Exercicio 10 Lista 3

--- Exercicios/JavaScript/Exercicios_Lista3/Lista3ex10.js
+++ Exercicios/JavaScript/Exercicios_Lista3/Lista3ex10.js
@@ -1,11 +1,4 @@
-function somarATEn(N){
-    var x=0, soma =0;
-    while (x <= N){
-        soma += x;
-        x++;
-    }
-return soma
-}
+import { somarATEn } from "../lib/biblioteca.js"
 console.log(somarATEn(5))
 console.log(somarATEn(10))
 console.log(somarATEn(1))
